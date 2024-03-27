88ec9b051acb3c52ca037570b857e45d9a3700e1
HichamHanane <hichamhanane2000@gmail.com>
2024-03-27 13:10:48 +0100

    update my portfolio

--- Skills/skill.js
+++
@@ -1,9 +0,0 @@
-AOS.init();
-
-let skills_slideBtn = document.getElementById('skills-slide-btn');
-
-
-
-skills_slideBtn.onclick=()=>{
-    document.querySelector('.skills-above-box').classList.add('slide-animation');
-}
